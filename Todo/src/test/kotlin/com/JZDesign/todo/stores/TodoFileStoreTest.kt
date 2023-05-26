ef93c4784d7d4bf951db33a05895b3cd3d4bca44
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-05-26 06:53:43 -0500

    Clean up a little more

--- Todo/src/test/kotlin/com/JZDesign/todo/stores/TodoFileStoreTest.kt
+++ Todo/src/test/kotlin/com/JZDesign/todo/stores/TodoFileStoreTest.kt
@@ -1,3 +1,5 @@
+import TodoFileStore.Companion.newTodo
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.readValue
@@ -5,6 +7,12 @@ import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+import java.util.TimeZone
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.TestInstance
 
@@ -12,6 +20,7 @@ import org.junit.jupiter.api.TestInstance
 class TodoFileStoreTest {
 
     val subject = TodoFileStore()
+
     @AfterAll
     fun tearDown() {
         TodoFileStore.cleanUp()
@@ -25,9 +34,9 @@ class TodoFileStoreTest {
     @Test
     fun `getAll returns a populated list for a given user`() {
         val todos = listOf(
-            TodoStorageObject("1"),
-            TodoStorageObject("2"),
-            TodoStorageObject("3"),
+            newTodo("1", "content"),
+            newTodo("2", "content"),
+            newTodo("3", "content"),
         )
 
         todos.forEach { subject.save(it, 9) }
@@ -42,27 +51,32 @@ class TodoFileStoreTest {
 
     @Test
     fun `get by id only returns the matching object in the store`() {
-        subject.save(TodoStorageObject("my-todo-id-1"), 1)
-        subject.save(TodoStorageObject("my-todo-id-2"), 1)
-        subject.save(TodoStorageObject("my-todo-id-3"), 1)
+        val firstTodo = newTodo("my-todo-id-1", "some todo title")
+
+        subject.save(firstTodo, 1)
+        subject.save(newTodo("my-todo-id-2", ""), 1)
+        subject.save(newTodo("my-todo-id-3", ""), 1)
+
         assertThat(subject.get("my-todo-id-1", 1))
-            .isEqualTo(TodoStorageObject("my-todo-id-1"))
+            .isEqualTo(firstTodo)
     }
 }
 
 class TodoFileStore {
 
-    fun getAllForUser(userId: Int): List<TodoStorageObject> = try {
-        serializer.readValue(fileFor(userId))
-    } catch (e: Exception) {
-        emptyList()
-    }
+    fun getAllForUser(userId: Int): List<TodoStorageObject> =
+        try {
+            serializer.readValue(fileFor(userId))
+        } catch (e: Exception) {
+            emptyList()
+        }
 
-    fun get(todoId: String, userId: Int): TodoStorageObject? = try {
-        getAllForUser(userId).firstOrNull { it.id == todoId }
-    } catch (e: Exception) {
-        null
-    }
+    fun get(todoId: String, userId: Int): TodoStorageObject? =
+        try {
+            getAllForUser(userId).firstOrNull { it.id == todoId }
+        } catch (e: Exception) {
+            null
+        }
 
     fun save(todoStorageObject: TodoStorageObject, userId: Int) {
         val todos: List<TodoStorageObject> = getAllForUser(userId)
@@ -90,7 +104,27 @@ class TodoFileStore {
             File(rootDirectory()).deleteRecursively()
         }
 
+        fun newTodo(id: String, content: String): TodoStorageObject {
+            val time = OffsetDateTime.now()
+            return TodoStorageObject(id, content, false, time, time)
+        }
+
     }
 }
 
-data class TodoStorageObject(val id: String)
+data class TodoStorageObject(
+    val id: String,
+    val content: String,
+    val completed: Boolean,
+    val createdAt: OffsetDateTime,
+    val updatedAt: OffsetDateTime
+) {
+    override fun equals(other: Any?): Boolean =
+        if (other is TodoStorageObject) {
+            other.id == id
+                    && other.content == content
+                    && other.completed == completed
+                    && other.createdAt.toEpochSecond() == createdAt.toEpochSecond()
+                    && other.updatedAt.toEpochSecond() == updatedAt.toEpochSecond()
+        } else false
+}
