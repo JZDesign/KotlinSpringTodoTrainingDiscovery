94c5228ddb86bfab1021474b8ad3dfe76cade8e1
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-05-26 07:13:17 -0500

    Add update happy path tests

--- Todo/src/test/kotlin/com/JZDesign/todo/stores/TodoFileStoreTest.kt
+++ Todo/src/test/kotlin/com/JZDesign/todo/stores/TodoFileStoreTest.kt
@@ -1,5 +1,4 @@
 import TodoFileStore.Companion.newTodo
-import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.readValue
@@ -7,14 +6,11 @@ import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.io.File
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 import java.time.OffsetDateTime
-import java.time.format.DateTimeFormatter
-import java.util.TimeZone
+import kotlin.IllegalStateException
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertThrows
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TodoFileStoreTest {
@@ -39,7 +35,7 @@ class TodoFileStoreTest {
             newTodo("3", "content"),
         )
 
-        todos.forEach { subject.save(it, 9) }
+        todos.forEach { subject.create(it, 9) }
 
         assertThat(subject.getAllForUser(9).count()).isEqualTo(3)
     }
@@ -53,12 +49,41 @@ class TodoFileStoreTest {
     fun `get by id only returns the matching object in the store`() {
         val firstTodo = newTodo("my-todo-id-1", "some todo title")
 
-        subject.save(firstTodo, 1)
-        subject.save(newTodo("my-todo-id-2", ""), 1)
-        subject.save(newTodo("my-todo-id-3", ""), 1)
+        subject.create(firstTodo, 1)
+        subject.create(newTodo("my-todo-id-2", ""), 1)
+        subject.create(newTodo("my-todo-id-3", ""), 1)
 
         assertThat(subject.get("my-todo-id-1", 1))
             .isEqualTo(firstTodo)
+    }
+
+    @Test
+    fun `create throws a CollisionException when a todo with the same ID comes in`() {
+        subject.create(newTodo("12345", ""), 1)
+
+        assertThrows<TodoFileStore.CollisionException> {
+            subject.create(newTodo("12345", ""), 1)
+        }
+    }
+
+    @Test
+    fun `update only updates changed fields`() {
+        val todo = newTodo("1", "")
+        val updateTime = OffsetDateTime.now()
+        subject.create(todo, userId = 1)
+        subject.update(UpdateTodoStorageObject("1", updateTime, "new content"), userId = 1)
+        assertThat(subject.get("1", 1)).isEqualTo(todo.copy(updatedAt = updateTime, content = "new content"))
+
+
+        val newUpdatedTime = OffsetDateTime.now()
+        subject.update(UpdateTodoStorageObject("1", newUpdatedTime, completed = true), userId = 1)
+        assertThat(subject.get("1", 1)).isEqualTo(
+            todo.copy(
+                updatedAt = newUpdatedTime,
+                content = "new content",
+                completed = true
+            )
+        )
     }
 }
 
@@ -78,9 +103,24 @@ class TodoFileStore {
             null
         }
 
-    fun save(todoStorageObject: TodoStorageObject, userId: Int) {
+    fun create(todoStorageObject: TodoStorageObject, userId: Int) {
         val todos: List<TodoStorageObject> = getAllForUser(userId)
+        if (todos.map { it.id }.contains(todoStorageObject.id)) throw CollisionException()
         fileFor(userId).writeText(serializer.writeValueAsString(todos.plus(todoStorageObject).toSet()))
+    }
+
+    fun update(updateObject: UpdateTodoStorageObject, userId: Int) {
+        val todos = getAllForUser(userId).toMutableList()
+        val todo = todos.first { it.id == updateObject.id }
+        todos.remove(todo)
+        todos.add(
+            todo.copy(
+                content = updateObject.content ?: todo.content,
+                completed = updateObject.completed ?: todo.completed,
+                updatedAt = updateObject.updatedAt
+            )
+        )
+        fileFor(userId).writeText(serializer.writeValueAsString(todos.toSet()))
     }
 
     companion object {
@@ -110,6 +150,8 @@ class TodoFileStore {
         }
 
     }
+
+    class CollisionException : IllegalStateException()
 }
 
 data class TodoStorageObject(
@@ -128,3 +170,11 @@ data class TodoStorageObject(
                     && other.updatedAt.toEpochSecond() == updatedAt.toEpochSecond()
         } else false
 }
+
+
+data class UpdateTodoStorageObject(
+    val id: String,
+    val updatedAt: OffsetDateTime,
+    val content: String? = null,
+    val completed: Boolean? = null,
+)
